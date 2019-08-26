845b50b7902ea078888ec487f3bd23f9e511b7b5
bologno <alexissoko@gmail.com>
2019-08-26 15:04:52 -0700

    about view added, templates oop. And url+view test scripts for 200 hhtp

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# pages/views.py
+from django.views.generic import TemplateView
 
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
